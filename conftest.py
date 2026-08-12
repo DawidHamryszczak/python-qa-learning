9e94a8ad5a3e74d74cda4060fe65967e2e087ed5
Dawid Hamryszczak <dhamryszczak03@gmail.com>
2026-08-12 12:36:55 +0200

    Add fixtures file and optimalize tests code

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def driver():
+    #STWORZENIE SETUPU KTORY ZAWSZE ZROBI SIE PRZED TESTEM ZEBY NIE PISAC TEGO 10000 RAZY
+    browser = webdriver.Edge()
+    browser.implicitly_wait(10)  # Czekaj maksymalnie 10 sekund na zaladowanie elementu
+
+    yield browser #Przekazanie obiektu browser do testu, wszytsko powyzej dzieje sie przed testem, wszystko ponizej dzieje sie po teście
+
+    browser.quit() #Zamkniecie przegladarki po zakonczeniu testu
--- test/test_login_pom.py
+++ test/test_login_pom.py
@@ -1,16 +1,21 @@
 from selenium import webdriver
 from pages.login_page import LoginPage
 
-def test_successful_login():
+def test_successful_login(driver):
 
-    #STWORZENIE DRIVERA
-    driver = webdriver.Edge()
     #STWORZENIE ELEMENTU login_page POD KTORYM KRYJE SIE KLASA LoginPage
     login_page = LoginPage(driver)
-
     login_page.open()
     login_page.login("tomsmith", "SuperSecretPassword!")
 
-    message_text = login_page.get_flash_message()
+    assert "You logged into a secure area!" in login_page.get_flash_message()
 
-    assert "You logged into a secure area!" in message_text
+def test_invalid_login(driver):
+
+    #STWORZENIE ELEMENTU login_page POD KTORYM KRYJE SIE KLASA LoginPage
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.login("invalid_user", "invalid_password")
+    login_page.get_flash_message()
+
+    assert "Your username is invalid!" in login_page.get_flash_message()
